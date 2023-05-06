03700140119974feceeba02e51257885f46d70c7
mmoghal <mmoghal29@gmail.com>
2023-05-05 20:44:55 -0400

    Set up Express to handle data parsing

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+
+// Set up Express to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
