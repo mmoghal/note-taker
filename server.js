51cb66dbcc5c64dea3ae0c4a9bf0db3c4d3ab1d1
mmoghal <mmoghal29@gmail.com>
2023-05-05 20:47:42 -0400

    Defined API routes

--- server.js
+++ server.js
@@ -4,9 +4,28 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const notes = require('./db/db.json');
 
 
 // Set up Express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+// API routes
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes.slice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
