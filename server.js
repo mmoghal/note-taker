6b2414a1287f85e3c25256da3598c452a2f3ef62
mmoghal <mmoghal29@gmail.com>
2023-05-07 12:49:52 -0400

    Updated server.js added delete and check function Updated code comments cleared existing db

--- server.js
+++ server.js
@@ -1,37 +1,41 @@
+// import necessary modules
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+
+// create an instance of express
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// import the array of notes from db.json
 const allnotes = require('./db/db.json');
-
 
 // Set up Express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// API routes
-
+// GET route to display all notes
 app.get('/api/notes', (req, res) => {
     res.json(allnotes.slice(1));
 });
 
+// GET route to display homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// GET route to display notes page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+// Catch-all route to redirect to homepage if requested resource doesn't exist
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-
-// Code to save a new
+// Function to create a new note
 function createNewNote(body, notesArray) {
     const newNote = body;
     if (!Array.isArray(notesArray))
@@ -51,13 +55,14 @@ function createNewNote(body, notesArray) {
     return newNote;
 }
 
+// POST route to add a new note
 app.post('/api/notes', (req, res) => {
     const newNote = createNewNote(req.body, allnotes);
     res.json(newNote);
 });
 
-// Function to delete note
-function deletenote(id, notesArray) {
+// Function to delete a note
+function deleteNote(id, notesArray) {
     for (let i = 0; i < notesArray.length; i++) {
         let note = notesArray[i];
 
@@ -71,11 +76,12 @@ function deletenote(id, notesArray) {
             break;
         }
     }
+    
 }
 
-// Code to delete a note
+// DELETE route to delete a note with given id
 app.delete('/api/notes/:id', (req, res) => {
-    deletenote(req.params.id, allnotes);
+    deleteNote(req.params.id, allnotes);
     res.json(true);
 });
 
